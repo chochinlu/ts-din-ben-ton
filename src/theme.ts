e54d9886fc49d6e8fd400d289805051bba8e2be8
Park.Lu <park.lu@nogle.com>
2020-09-01 11:15:10 +0800

    init

---
+++ src/theme.ts
@@ -0,0 +1,19 @@
+const theme = {
+  fontSizes: [12, 14, 16, 24, 32, 48, 64],
+  colors: {
+    primary: '#07c',
+    gray: '#f6f6ff',
+  },
+  buttons: {
+    primary: {
+      color: 'white',
+      bg: 'primary',
+    },
+    outline: {
+      color: 'primary',
+      bg: 'transparent',
+      boxShadow: 'inset 0 0 0 2px',
+    },
+  },
+}
+export default theme
